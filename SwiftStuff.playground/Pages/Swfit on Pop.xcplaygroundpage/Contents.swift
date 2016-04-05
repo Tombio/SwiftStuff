8929c62a111e0443af01992fe6760aa117b1bf38
Tombio <tomijlahtinen@gmail.com>
2016-04-05 17:59:48 +0300

    Added Playground and stuff

---
+++ SwiftStuff.playground/Pages/Swfit on Pop.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+/*:
+ # Swift on POP
+ Protocol Oriented Programming
+ 
+ */
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+//: Playground - noun: a place where people can play
+
+//: [Next](@next)
---
+++ SwiftStuff.playground/Pages/Syntaksi.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: # Syntaksi
+
+import Foundation
+
+// Eksplisiittinen arvo
+let explicitInt: Int = 10
+
+// Implisiittinen arvo
+let implicitInt = 42
+
+let str = "Foobar"
+
+// [NSObject]
+let arr = ["foo", "bar", 1 , 2]
+
+// Iteraatio
+for (index, value) in arr.enumerate() {
+    print("\(index) => \(value)")
+}
+
+
+protocol proto {}
+struct rakenne: proto {}
+class luokka: rakenne {}
+//: [Previous](@previous) | [Next](@next)
